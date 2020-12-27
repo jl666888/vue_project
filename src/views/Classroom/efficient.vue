783729b5e4e22687994c53e1753a6ee2ea9c4003
侯德康 <2163203830@qq.com>
2020-12-27 13:57:31 +0800

    v1.6

--- src/views/Classroom/efficient.vue
+++ src/views/Classroom/efficient.vue
@@ -6,13 +6,7 @@
     <van-loading style="text-align:center" v-show="show" />
 
     <div class="to">
-      
-      <van-nav-bar
-        :title="msg"
-        left-text="返回"
-        left-arrow
-        @click-left="back"
-      />
+      <van-nav-bar :title="msg" left-text="返回" left-arrow @click-left="back" />
     </div>
     <div style="margin-top:20px">
       <van-button class="go" type="primary" block @click="go" color="#f1f1f1">搜索</van-button>
@@ -55,21 +49,25 @@ export default {
     this.$store.commit("setShow", false);
     let id = this.$route.query.id;
     this.id = id;
-    this.$http.get("https://www.fastmock.site/mock/e69183d9c2316e1982d198fe4e7d1d57/school/getList").then(ret => {
-      this.arr = ret.data;
-      this.msg = this.$store.state.grid[this.id];
-      this.$store.commit("setTitle", this.arr);
-      //   console.log(ret);
-      if (this.arr.length > 0) {
-        this.show = false;
-      }
-    });
+    this.$http
+      .get(
+        "https://www.fastmock.site/mock/e69183d9c2316e1982d198fe4e7d1d57/school/getList"
+      )
+      .then(ret => {
+        this.arr = ret.data;
+        this.msg = this.$store.state.grid[this.id];
+        this.$store.commit("setTitle", this.arr);
+        //   console.log(ret);
+        if (this.arr.length > 0) {
+          this.show = false;
+        }
+      });
   },
 
   methods: {
     go: function() {
       // console.log("这是搜索按钮,请跳转搜索路由组件");
-      this.$router.push('/classroom/search')
+      this.$router.push("/classroom/search");
     },
     back: function() {
       this.$router.go(-1);
@@ -101,6 +99,5 @@ export default {
 }
 .go {
   margin-top: 60px;
-  
 }
 </style>
